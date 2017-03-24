f3f32988214567b78824430bace61d7b1830353d
AndyLiu <sef698524@gmail.com>
2017-03-25 02:59:32 +0800

    fix some problem

--- HW_1/run1_a.m
+++ HW_1/run1_a.m
@@ -1,8 +1,19 @@
 src = imread('lalaland.png') ;
 
-n = 8; 
+n = 8  ; 
 src = im2double(src) ;
 [out1, out2] = DCT(src , n) ;
-psnr1 = computePSNR(src , out2) ;
+
+
+
+src = im2uint8(src);
+out1 = im2uint8(out1);
+out2 = im2uint8(out2);
+
+%imwrite(out1 ,'DCT8.png') ;
+%imwrite(out2 ,'IDCT8.png') ;
+%imshow(out1);
+
+psnr1 = computePSNR(out2, src) ;
 disp(psnr1) ;
-imwrite(out2 ,'DCT.png') ;
+
--- HW_1/run3_a.m
+++ HW_1/run3_a.m
@@ -1,3 +1,7 @@
-src3 = imread('img_HR.png') ;
-u =  NN(src2) ;  
-imshow(u) ;
+src3 = imread('img_LR.png') ;
+src4 = imread('img_HR.png') ;
+u =  NN(src3) ;  
+c = imresize(src3, 4, 'nearest');
+psnr1 = computePSNR(src4 ,u);
+disp(psnr1) ;
+%imshow(u) ;
