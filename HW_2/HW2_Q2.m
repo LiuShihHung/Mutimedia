42ae88f39fc5242229caa47f97f150d7a8ec80b2
AndyLiu <sef698524@gmail.com>
2017-04-14 04:42:07 +0800

    finish

--- HW_2/HW2_Q2.m
+++ HW_2/HW2_Q2.m
@@ -11,7 +11,12 @@ audio_files = {'audio/guitar_01.wav', 'audio/guitar_02.wav', 'audio/guitar_03.wa
                'audio/piano_01.wav', 'audio/piano_02.wav', 'audio/piano_03.wav', ...
                'audio/violin_01.wav', 'audio/violin_02.wav', 'audio/violin_03.wav', ...
                'audio/drum_01.wav', 'audio/drum_02.wav', 'audio/drum_03.wav'};
-
+           
+output_files = {'guitar_01.png', 'guitar_02.png', 'guitar_03.png', ...
+               'piano_01.png', 'piano_02.png', 'piano_03.png', ...
+               'violin_01.png', 'violin_02.png', 'violin_03.png', ...
+               'drum_01.png', 'drum_02.png', 'drum_03.png'};
+           
 class_num = length(class_labels);
 instance_num = length(audio_files) / class_num;
 cut_duration = 3;
@@ -32,6 +37,7 @@ for i = 1 : length(audio_files)
     % Hint: imwrite
     % Name the file 'InstrumentName_para.png'
     % para means the serial number, i.e. guitar_01.png for guitar_01.wav
+     imwrite(spectrogram_images{i},output_files{i});
 end
 
 %% Load the test spectrogram of the test audio file
